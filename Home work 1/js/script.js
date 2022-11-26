f9ebbed94155b1f7c6749a59dec0765d6fe03451
Andrew10x10 <104031643+Andrew10x10@users.noreply.github.com>
2022-11-27 00:57:41 +0200

    Add

---
+++ Home work 1/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let user = prompt('Hey,bro.What is your name?');
+
+alert('Hello,' + user + '.Nice to meet you');
+
+let age = prompt( 'How old are you?');
+
+alert('Hm...' + age + ',you really old');
