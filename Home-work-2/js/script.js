dbe688ad7b0e7240fadf6e127f4e5651afe22b21
Andrew10x10 <104031643+Andrew10x10@users.noreply.github.com>
2022-11-27 16:13:20 +0200

    Add

---
+++ Home-work-2/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+let name = 'John';
+let admin = name;
+
+alert(admin);
+
+let planet = 'Earth';
+
+let user = prompt('what is your name');
+alert('hello,' + user);
+
+let number = '10' + 10;
+console.log(typeof(number));
