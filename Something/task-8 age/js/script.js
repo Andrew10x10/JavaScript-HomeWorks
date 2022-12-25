305d5f8bb0f7430a64c3094f7764351d62fda603
Andrew10x10 <104031643+Andrew10x10@users.noreply.github.com>
2022-12-25 04:00:54 +0200

    update

--- Something/task-8 age/js/script.js
+++ Something/task-8 age/js/script.js
@@ -1,20 +1,19 @@
 'use strict';
-
 function getAge(number) {
-    if (number == 0)
+    if (number === 0)
         return 'лет'
-    if (number >= 5 && number <= 10 )
+    if (number >= 5 && number <= 10)
         return 'лет'
     number %= 10
 
-    if (number == 1)
+    if (number === 1)
         return "год"
     if (number >= 2 && number <= 4)
         return "года"
     return 'лет'
 }
-
-
-let num = +prompt('here number');
-alert("вам " + num + ' ' + getAge(num));
+const now = new Date().getFullYear();
+let yearOfBorn = +(prompt('Введите ваш год рождения'));
+yearOfBorn = now - yearOfBorn;
+alert('На данный момент вам ' + yearOfBorn + getAge(yearOfBorn));
 
