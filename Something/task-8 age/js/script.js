8efb39cd9a8674f57cd9e545fec6ed818ea3feec
Andrew10x10 <104031643+Andrew10x10@users.noreply.github.com>
2022-12-28 02:35:15 +0200

    add  dz

--- Something/task-8 age/js/script.js
+++ Something/task-8 age/js/script.js
@@ -1,21 +1,32 @@
+// // const now = new Date().getFullYear();
+// let yearOfBorn = (prompt('Введите ваш год рождения'));
+// // // yearOfBorn = now - yearOfBorn;
+// alert('На данный момент вам ' + yearOfBorn + getAge(yearOfBorn));
+
 'use strict';
+const error = 'ошибка ввода данных';
 
 function getAge(number) {
-    if (number === 0)
-        return 'лет'
-    if (number >= 5 && number <= 13)
-        return 'лет'
-    number %= 10
+    if (isNaN(number) || number === '' || number === null) {
+        return error;
+    } else {
+        if (number === 0)
+            return 'лет'
+        if (number >= 5 && number <= 13)
+            return 'лет'
+        number %= 10
 
-    if (number === 1)
-        return "год"
-    if (number >= 2 && number <= 4)
-        return "года"
-    return 'лет'
+        if (number === 1)
+            return "год"
+        if (number >= 2 && number <= 4)
+            return "года"
+    }
 }
 
-const now = new Date().getFullYear();
-let yearOfBorn = +(prompt('Введите ваш год рождения'));
-yearOfBorn = now - yearOfBorn;
-alert('На данный момент вам ' + yearOfBorn + getAge(yearOfBorn));
-
+let yearOfBorn = +prompt('Введите ваш возраст');
+const moment = 'На данный момент вам ' + yearOfBorn + ' ' + getAge(yearOfBorn)
+if (getAge(yearOfBorn) === error) {
+    alert(error)
+} else {
+    alert(moment)
+}
