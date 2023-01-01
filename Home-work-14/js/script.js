9344956e08ce6e6666380edb6466f11ce3228a31
Andrew10x10 <104031643+Andrew10x10@users.noreply.github.com>
2023-01-01 22:06:29 +0200

    update dz

--- Home-work-14/js/script.js
+++ Home-work-14/js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+//1
+// for (let i = 1; i <= 100; i++){
+//     console.log(i)
+// }
+
+//2
+// for (let i = 100; i >= 1;i--) {
+//         console.log(i)
+// }
+
+//3
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+//4
+// const arr = [1, 2, 3, 4, 5];
+// let arrSum = 0;
+// for (let a = 0; a < arr.length; a++) {
+//     arrSum = arrSum + arr[a];
+// }
+// console.log(arrSum)
+
+// 5
+
+// const arr = [1, 2, 3, 4, 5];
+// let arrSum = 0;
+// for (let a = 0; a < arr.length; a++) {
+//     arrSum += arr[a]**2;
+// }
+// console.log(arrSum)
+
