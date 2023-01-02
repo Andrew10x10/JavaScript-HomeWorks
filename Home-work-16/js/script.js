5211fbe6149d9c7166c01fdeba64187d24f98d25
Andrew10x10 <104031643+Andrew10x10@users.noreply.github.com>
2023-01-02 21:25:04 +0200

    update dz

--- Home-work-16/js/script.js
+++ Home-work-16/js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+let userNumber = prompt('number user')
+let result = null;
+
+if (userNumber <= 2) {
+    result = 'error';
+}
+if (userNumber !== null && !isNaN(+userNumber)) {
+    for (let i = 2; i < userNumber; i++) {
+        if (userNumber % i === 0) {
+            result = `${i} явлеется найменшим делителем ${userNumber}`
+            break;
+        } else {
+            result = `${userNumber} Это простое число`;
+        }
+        console.log(i);
+    }
+} else {
+    result = 'Что-то пошло не по плану , попробуй снова';
+}
+console.log(result,userNumber);
+alert(result);
