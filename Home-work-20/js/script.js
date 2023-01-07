6485c1c0ba92b46c84559fbabc24fc8e4c25ef6c
Andrew10x10 <104031643+Andrew10x10@users.noreply.github.com>
2023-01-07 23:25:34 +0200

    new dz 'borscht'

--- Home-work-20/js/script.js
+++ Home-work-20/js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+const onePotato = 0.075;
+const fourPotato = 4 * onePotato;
+const oneLiterBorscht = fourPotato;
+const amountOfBorscht = +prompt('How much do you want to cook liter Ukrainian Borscht', '48');
+
+const amountOfBorscht2 = oneLiterBorscht * amountOfBorscht
+
+console.log('you need ' + amountOfBorscht2 + ' kilogram of potatoes to make ' + amountOfBorscht + ' liters of borscht');
+const buyPlace = prompt('where do you want to buy potatoes (privoz) or (tavria)','privoz')
+
+const oneKgPotatoMall = 13;
+const sumUAHPotatoes = oneKgPotatoMall * amountOfBorscht2
+
+switch (buyPlace) {
+    case 'privoz':
+        console.log('Privoz price potato: ' + Math.floor(sumUAHPotatoes) + 'uah')
+        console.log('Tavria price potato: ' + Math.ceil(sumUAHPotatoes) + 'uah')
+        break;
+    case 'tavria':
+        console.log('Tavria price potato: ' + Math.ceil(sumUAHPotatoes) + 'uah')
+        console.log('Privoz price potato: ' + Math.floor(sumUAHPotatoes) + 'uah')
+        break;
+}
